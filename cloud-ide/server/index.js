52f56bba1377c9579aae8a11dcc6f25beaf6602d
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-09-20 16:15:13 +0500

    Initialize server package with essential dependencies and configuration

---
+++ cloud-ide/server/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const express = require("express");
+const { Server: SocketServer } = require("socket.io");
+const pty = require("node-pty-prebuilt-multiarch");
+const os = require("os");
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let shell;
+if (os.platform() === "win32") {
+  shell = "C:\\Program Files\\Git\\bin\\bash.exe";
+} else {
+  shell = "bash";
+}
+
+const ptyProcess = pty.spawn(shell, [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD || process.cwd(),
+  env: process.env,
+});
+
+io.on("connection", (socket) => {
+  console.log("✅ A user connected:", socket.id);
+
+  socket.on("terminal:write", (data) => {
+    ptyProcess.write(data + "\r");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ User disconnected:", socket.id);
+  });
+});
+
+ptyProcess.onData((data) => {
+  io.emit("terminal:data", data);
+});
+
+server.listen(9000, () => {
+  console.log("🚀 Server is running on port 9000");
+});
