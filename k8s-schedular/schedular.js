1fc85bccf3f1a033b7b706a925ce7e2acf98aab7
taimoor115 <taimoorhussain985@gmail.com>
2026-04-26 23:22:15 +0500

    feat: add Docker integration and new job scheduler
    - Added `dockerode` dependency for Docker interactions.
    - Introduced a new job scheduler `jobsScehdular` for handling CRI jobs.
    - Implemented `jobCRIWorker` to manage job processing, including Docker image checks and container management.
    - Enhanced logging for better traceability during job execution.

--- k8s-schedular/schedular.js
+++ k8s-schedular/schedular.js
@@ -1,10 +1,13 @@
-import { jobDispatchSchedular } from "./queues/queues.js";
-import { jobDispatchWorker } from "./worker/index.js";
+import { jobDispatchSchedular, jobsScehdular } from "./queues/queues.js";
+import { jobCRIWorker, jobDispatchWorker } from "./worker/index.js";
 async function init() {
   Promise.all([
     jobDispatchSchedular.upsertJobScheduler("job-dispatcher", {
       every: 1000 * 2, // 2s
     }),
+    jobsScehdular.upsertJobScheduler("jobs-cri-schedular", {
+      every: 1000 * 10, // 10s
+    }),
   ]);
 }
-init()
+init();
